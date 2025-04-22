ff4a445fbfdb1047784a133c760b4e4085171260
EzechielKiregha <ekiregha@gmail.com>
2025-04-22 16:39:32 +0200

    prisma generator modified

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { Country, FreelanceStatus, KycStatus, MediaType, PaymentMethod, PaymentStatus, PrismaClient, RechargeMethod } from '../src/generated/prisma';
+import { Country, FreelanceStatus, KycStatus, MediaType, PaymentMethod, PaymentStatus, PrismaClient, RechargeMethod } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
